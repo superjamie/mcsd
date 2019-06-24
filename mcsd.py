f9141a2ce253d8ad36495a96bc7e94aac5611f4b
Jamie Bainbridge <jamie.bainbridge@gmail.com>
2019-06-24 12:44:49 +1000

    add argparse, modularise

--- mcsd.py
+++ mcsd.py
@@ -1,29 +1,52 @@
 #!/usr/bin/python3
 
 from scapy.all import *
+import argparse
 import ipaddress
 import os
 import sys
 
+pcap_packets = PacketList()  # list of raw packets from the pcap file
+filt_packets = PacketList()  # list of packets after filtering
+
 if __name__ == "__main__":
 
-    try:
-        filename = sys.argv[1]
-    except IndexError:
-        print("Usage:", os.path.basename(__file__), "[filename]")
-        exit(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="pcap filename to read")
+    parser.add_argument("-s", "--source", help="filter on source IP address", action="store_true")
+    parser.add_argument("-d", "--dest"  , help="filter on destination IP address", action="store_true")
+    parser.add_argument("-S", "--sport" , help="filter on source port number", action="store_true")
+    parser.add_argument("-D", "--dport" , help="filter on destination port number", action="store_true")
+    parser.add_argument("-o", "--offset", help="offset into packet to start reading", action="store_true")
+    parser.add_argument("-l", "--length", help="length of characters to read", action="store_true")
+    parser.add_argument("-r", "--raw"   , help="raw mode. only print payload data")
+    args = parser.parse_args()
 
     try:
-        pkts = rdpcap(filename)
+        pcap_packets = rdpcap(args.filename)
     except FileNotFoundError:
         print("File not found:", filename)
         exit(1)
 
-    # same to assume all packets are IP packets? probably not
-    for pkt in pkts:
-        if (IP in pkt) and (ipaddress.ip_address(pkt[IP].dst) in ipaddress.ip_network("224.0.0.0/4")):
-                if (UDP in pkt) and (pkt[UDP].len > 0):
-                    print(pkt[Raw].load[0:8].decode("utf-8"))
+    # filter
+    # TODO: filter on src/dst arguments
+    for packet in pcap_packets:
+        if (IP in packet) and (ipaddress.ip_address(packet[IP].dst) in ipaddress.ip_network("224.0.0.0/4")):
+            if (UDP in packet) and (packet[UDP].len > 0):
+                filt_packets.append(packet)
+
+    # print header
+    # TODO: not in raw mode
+    if (len(filt_packets) > 0):
+        print("{:10s} {:s}".format("frame no", "payload"))
+
+    # process
+    # TODO: read payload using offset and len arguments
+    # TODO: don't print framenumber in raw mode
+    for packet in filt_packets:
+        framenumber = pcap_packets.index(packet)+1
+        payload = packet[Raw].load[0:8].decode("utf-8")
+        print("{:10d} {:s}".format(framenumber, payload))
 
 # vim: ft=python
 
