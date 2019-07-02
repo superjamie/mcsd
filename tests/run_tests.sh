4c4ef1248182f984d3d67c0da7c52a4e9dc19d4c
Jamie Bainbridge <jamie.bainbridge@gmail.com>
2019-07-02 18:05:47 +1000

    complete tests and make repeatable

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,6 +6,12 @@ num_tests=${#tests[@]}
 #echo "tests = ${tests[@]}."
 #echo "num_tests = $num_tests"
 
+./generator.py
+if [[ $(tshark -nr mcpackets.pcap -T fields -e frame.number -e ip.src -e ip.dst -e ip.proto -e udp.srcport -e udp.dstport -e udp.length | diff - mcpackets.txt) ]]; then
+    echo "ERROR - packet generator not ok"
+    exit 1
+fi
+
 echo "1..$num_tests"
 test_num=0
 for test_prog in ${tests[@]}; do
@@ -16,4 +22,8 @@ for test_prog in ${tests[@]}; do
 		echo "$test_num not ok $test_prog $@"
 	fi
 done
+
+if [[ -f mcpackets.pcap ]]; then
+    rm -f mcpackets.pcap
+fi
 
